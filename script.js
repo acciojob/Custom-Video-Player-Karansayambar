d61cf31272f67b8752db65ed1923f268ae098592
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-14 11:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,50 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// const prev = document.querySelector(".player__button");
+// const next = document.querySelector(".player__button");
+
+ranges.forEach(range => {
+	range.addEventListener("input", () => {
+		if(range.name === "volume"){
+			video.volume = range.value;
+		}else if(range.name === "playbackRate"){
+			video.playbackRate = range.value
+		}
+	})
+})
+
+video.addEventListener("timeupdate", () => {
+    const progressPer = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${progressPer}%`;
+});
+
+skipButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		const skipAmount = parseFloat(button.dataset.skip);
+		video.currentTime = skipAmount;
+	})
+})
+
+toggle.addEventListener("click", () => {
+	if(video.paused){
+		video.play()
+	}else{
+		video.pause();
+	}
+});
+
+
+
+// next.addEventListener("click", () => {
+//     if (video.currentTime + 2 <= video.duration) {
+//         video.currentTime += 2;
+//     }
+// });
+
+// prev.addEventListener("click" ,() => {
+// 	if(video.currentTime -2 >= 0){
+// 		video.currentTime -= 2;
+// 	}
+// })
